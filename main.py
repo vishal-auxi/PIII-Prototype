26eaa50e389df46d72525dda88afa243bbfae9ab
Vishal Annamaneni <annamaneni.v@husky.neu.edu>
2020-10-12 01:57:06 -0700

    Refactored code

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-# This is a sample Python script.
+from flask import Flask
+from flask import request
+from pptx import Presentation
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+def create_ppt(ppt_title):
+    print("in create_ppt")
+    prs = Presentation()
+    title_slide_layout = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(title_slide_layout)
+    title = slide.shapes.title
+    subtitle = slide.placeholders[1]
+
+    title.text = ppt_title
+    subtitle.text = "Created with Powerpoint Bot"
+
+    prs.save(ppt_title + ".pptx")
+    return
+
+
+@app.route("/", methods=['POST'])
+def ppt_request_handle():
+    print("in ppt_request_handle")
+    req = request.json
+    print(req)
+
+    if req["req"] == "create_ppt":
+        create_ppt(req["title"])
+    return f"Request successful", 200
+
+
+# 'Main' function to run
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)  # run server in debug mode
